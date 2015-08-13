311b6d10ef894c7759e148ef513796b72182a2cd
Serguei Katkov <serguei.i.katkov@intel.com>
2015-08-13 21:48:15 +0600

    ART-Extension: Enable aggregation of timing collection for optimizing
    To get per pass time stats you should specify the options
    --dump-timing --dump-passes to dex2oatd.
    
    Change-Id: Ibf20d21629fcc61d9d0972c3239e80429194953e
    Category: device enablement
    Domain: AOSP.ART-ME
    Origin: internal
    Upstream-Candidate: no, not-aosp
    Orig-Tracked-On: https://jira01.devtools.intel.com/browse/CAR-2472
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-1122
    Signed-off-by: Serguei Katkov <serguei.i.katkov@intel.com>
    (cherry picked from commit b9b1ebc97b02289682b74f4842f96c6713eac7b1)
    Reviewed-on: https://android.intel.com/594093
    Reviewed-by: Rajendran, Jaishankar <jaishankar.rajendran@intel.com>
    Tested-by: Rajendran, Jaishankar <jaishankar.rajendran@intel.com>

--- compiler/optimizing/optimizing_compiler.cc
+++ compiler/optimizing/optimizing_compiler.cc
@@ -150,7 +150,9 @@ class PassObserver : public ValueObject {
       : graph_(graph),
         cached_method_name_(),
         timing_logger_enabled_(compiler_driver->GetDumpPasses()),
+        timing_logger_per_method_enabled_(true),
         timing_logger_(timing_logger_enabled_ ? GetMethodName() : "", true, true),
+        compiler_driver_(compiler_driver),
         disasm_info_(graph->GetArena()),
         visualizer_oss_(),
         visualizer_output_(visualizer_output),
@@ -160,7 +162,7 @@ class PassObserver : public ValueObject {
         graph_in_bad_state_(false) {
     if (timing_logger_enabled_ || visualizer_enabled_) {
       if (!IsVerboseMethod(compiler_driver, GetMethodName())) {
-        timing_logger_enabled_ = visualizer_enabled_ = false;
+        timing_logger_per_method_enabled_ = visualizer_enabled_ = false;
       }
       if (visualizer_enabled_) {
         visualizer_.PrintHeader(GetMethodName());
@@ -171,8 +173,11 @@ class PassObserver : public ValueObject {
 
   ~PassObserver() {
     if (timing_logger_enabled_) {
-      LOG(INFO) << "TIMINGS " << GetMethodName();
-      LOG(INFO) << Dumpable<TimingLogger>(timing_logger_);
+      compiler_driver_->GetTimingsLogger()->AddLogger(timing_logger_);
+      if (timing_logger_per_method_enabled_) {
+        LOG(INFO) << "TIMINGS " << GetMethodName();
+        LOG(INFO) << Dumpable<TimingLogger>(timing_logger_);
+      }
     }
     DCHECK(visualizer_oss_.str().empty());
   }
@@ -259,7 +264,9 @@ class PassObserver : public ValueObject {
   std::string cached_method_name_;
 
   bool timing_logger_enabled_;
+  bool timing_logger_per_method_enabled_;
   TimingLogger timing_logger_;
+  CompilerDriver* compiler_driver_;
 
   DisassemblyInformation disasm_info_;
 
