3eab475e5bf99c27b78fcf9f112c031a8072aa4a
lzang1 <lin.zang@intel.com>
2016-01-27 16:41:46 +0800

    Parallel Copying for GSS/SS
    Implemented Parallel Coying for SS/GSS collector.
    Also enable TLAB by default, TLAB is reused as PLAB when
    conducting copying in GC.
    
    Our work show that the size of mark stack divided for parallel
    copy tasks is sensitive to performance, so this patch made an
    option "-XX:FirstIterCopySize=" for creating size-specified
    mark stack task as first iteration copying task, by default it is 10.
    
    Performance improvment:
    AndroidGCTest with GSS enabled TLAB:
    before: 88707 ms
    after: 64704 ms
    
    Change-Id: Ic6383561daa27addca54caf965d5b94327447be3
    Tracked-On: https://jira01.devtools.intel.com/browse/CAR-2006
    Category: device enablement
    Domain: AOSP.ART-GC
    Origin: upstream, https://android-review.googlesource.com/#/c/198855/
    Upstream-Candidate: no, upstream
    Signed-off-by: Lin Zang <lin.zang@intel.com>
    Signed-off-by: Serdjuk, Nikolay Y <nikolay.y.serdjuk@intel.com>
    Reviewed-on: https://android.intel.com/476264
    Reviewed-by: Popov, Ivan G <ivan.g.popov@intel.com>
    Reviewed-by: Beyler, Jean Christophe <jean.christophe.beyler@intel.com>
    Reviewed-by: Feng, Haitao <haitao.feng@intel.com>
    Tested-by: Popov, Ivan G <ivan.g.popov@intel.com>

--- runtime/gc/heap.h
+++ runtime/gc/heap.h
@@ -170,6 +170,7 @@ class Heap {
        size_t large_object_threshold,
        size_t parallel_gc_threads,
        size_t conc_gc_threads,
+       size_t first_iter_copy_size,
        bool low_memory_mode,
        size_t long_pause_threshold,
        size_t long_gc_threshold,
@@ -463,11 +464,12 @@ class Heap {
 
   void AddFinalizerReference(Thread* self, mirror::Object** object);
 
-  // Returns the number of bytes currently allocated.
   size_t GetBytesAllocated() const {
     return num_bytes_allocated_.LoadSequentiallyConsistent();
   }
-
+  size_t GetRevokeBytes() const {
+    return num_bytes_freed_revoke_.LoadSequentiallyConsistent();
+  }
   // Returns the number of objects currently allocated.
   size_t GetObjectsAllocated() const
       REQUIRES(!Locks::heap_bitmap_lock_);
@@ -540,9 +542,10 @@ class Heap {
   // Deflate monitors, ... and trim the spaces.
   void Trim(Thread* self) REQUIRES(!*gc_complete_lock_);
 
-  void RevokeThreadLocalBuffers(Thread* thread);
+  void RevokeThreadLocalBuffers(Thread* thread, bool record_free = true);
   void RevokeRosAllocThreadLocalBuffers(Thread* thread);
-  void RevokeAllThreadLocalBuffers();
+  void RevokeAllThreadLocalBuffers(bool record_free = true);
+  void AssertAllThreadLocalBuffersAreRevoked();
   void AssertThreadLocalBuffersAreRevoked(Thread* thread);
   void AssertAllBumpPointerSpaceThreadLocalBuffersAreRevoked();
   void RosAllocVerification(TimingLogger* timings, const char* name)
@@ -616,7 +619,6 @@ class Heap {
   space::RosAllocSpace* GetRosAllocSpace() const {
     return rosalloc_space_;
   }
-
   // Return the corresponding rosalloc space.
   space::RosAllocSpace* GetRosAllocSpace(gc::allocator::RosAlloc* rosalloc) const
       SHARED_REQUIRES(Locks::mutator_lock_);
@@ -670,6 +672,12 @@ class Heap {
   size_t GetConcGCThreadCount() const {
     return conc_gc_threads_;
   }
+  // For Parallel Copying collector.
+  // Get task size of the first iteration of copy task.
+  size_t GetFirstIterCopySize() const {
+    return first_iter_copy_size_;
+  }
+
   accounting::ModUnionTable* FindModUnionTableFromSpace(space::Space* space);
   void AddModUnionTable(accounting::ModUnionTable* mod_union_table);
 
@@ -1085,6 +1093,9 @@ class Heap {
 
   // How many GC threads we may use for unpaused parts of garbage collection.
   const size_t conc_gc_threads_;
+
+  // How many objects in the parallel copy task for first iteration.
+  const size_t first_iter_copy_size_;
 
   // Boolean for if we are in low memory mode.
   const bool low_memory_mode_;
