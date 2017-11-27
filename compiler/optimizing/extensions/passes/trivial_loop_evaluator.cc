9281a76fa35e9b528f2798bc4153d721256e99f5
Neeraj Solanki <neeraj.solanki@intel.com>
2017-11-27 13:37:39 +0530

    Description of Change: Fix for ART Unit Test : 8001-tle-InlineTests Limit of shift count should be determined by type of destination and not by type of  shift amount constant (right operand) of shift instruction
    Additional Test Cases Passing:
    test-art-host-run-test-debug-prebuild-optimizing-no-relocate-ntrace-cms-checkjni-picimage-npictest-ndebuggable-no-jvmti-8001-tle-InlineTests64
    test-art-host-run-test-debug-prebuild-speed-profile-no-relocate-ntrace-cms-checkjni-picimage-npictest-ndebuggable-no-jvmti-8001-tle-InlineTests6
    
    Additional Test Cases Failing:
    No additional failures
    
    Change-Id: I16965d5a8b87077780da2eb6cd301c10fe30b4fe
    Tracked-On:  https://jira01.devtools.intel.com/browse/CAR-4544
    Signed-off-by: Neeraj Solanki <neeraj.solanki@intel.com>
    Category: device enablement
    Domain: AOSP.ART-Other
    Origin: internal
    Upstream-Candidate: no, proprietary
    Reviewed-on: https://android.intel.com/609617
    Reviewed-by: Bodapati, Shalini Salomi <shalini.salomi.bodapati@intel.com>
    Reviewed-by: Kumar Gupta, Sanjiv <sanjiv.kumar.gupta@intel.com>
    Tested-by: Kumar Gupta, Sanjiv <sanjiv.kumar.gupta@intel.com>

--- compiler/optimizing/extensions/passes/trivial_loop_evaluator.cc
+++ compiler/optimizing/extensions/passes/trivial_loop_evaluator.cc
@@ -380,18 +380,18 @@ class TLEVisitor : public HGraphVisitor {
         Value(FpRem(left_value.d, right_value.d, [] (double x, double y) -> double { return std::fmod(x, y); }))));
   }
 
-  int32_t ComputeShiftCount(HInstruction* instr) {
-    int32_t shift_count = 0;
-    Primitive::Type type = instr->GetType();
-    if (type == Primitive::kPrimInt) {
-      shift_count = GetValue(instr).i & kMaxIntShiftDistance;
-    } else if (type == Primitive::kPrimLong) {
-      shift_count = GetValue(instr).i & kMaxLongShiftDistance;
-    } else {
-      SetError(instr);
-    }
-    return shift_count;
-  }
+  int32_t ComputeShiftCount(HBinaryOperation* shift_instr) {
+     int32_t shift_count = 0;
+    Primitive::Type type = shift_instr->GetLeft()->GetType();
+     if (type == Primitive::kPrimInt) {
+      shift_count = GetValue(shift_instr->GetRight()).i & kMaxIntShiftDistance;
+     } else if (type == Primitive::kPrimLong) {
+      shift_count = GetValue(shift_instr->GetRight()).l & kMaxLongShiftDistance;
+     } else {
+      SetError(shift_instr);
+     }
+     return shift_count;
+   }
 
   void VisitShl(HShl* instr) OVERRIDE {
     NOTHING_IF_ERROR;
@@ -399,7 +399,7 @@ class TLEVisitor : public HGraphVisitor {
     Value left_value = GetValue(instr->GetLeft());
     NOTHING_IF_ERROR;
 
-    int32_t shift_count = ComputeShiftCount(instr->GetRight());
+    int32_t shift_count = ComputeShiftCount(instr);
     NOTHING_IF_ERROR;
     SWITCH_FOR_TYPES(instr, instr->GetLeft()->GetType(),
       values_.Overwrite(instr, Value(left_value.i << shift_count)),
@@ -414,7 +414,7 @@ class TLEVisitor : public HGraphVisitor {
     Value left_value = GetValue(instr->GetLeft());
     NOTHING_IF_ERROR;
 
-    int32_t shift_count = ComputeShiftCount(instr->GetRight());
+    int32_t shift_count = ComputeShiftCount(instr);
     NOTHING_IF_ERROR;
 
     SWITCH_FOR_TYPES(instr, instr->GetLeft()->GetType(),
@@ -430,7 +430,7 @@ class TLEVisitor : public HGraphVisitor {
     Value left_value = GetValue(instr->GetLeft());
     NOTHING_IF_ERROR;
 
-    int32_t shift_count = ComputeShiftCount(instr->GetRight());
+    int32_t shift_count = ComputeShiftCount(instr);
     NOTHING_IF_ERROR;
 
     SWITCH_FOR_TYPES(instr, instr->GetLeft()->GetType(),
