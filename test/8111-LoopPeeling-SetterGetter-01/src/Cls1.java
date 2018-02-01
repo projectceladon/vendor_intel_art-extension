3178adde9372b94f4be8deb30cb3657d4f2e513b
Anuvarshini BC <anuvarshini.bc@intel.com>
2018-02-01 19:47:40 +0530

    Adding tests for Loop Peeling. There are no failures seen.
    Change-Id: I8781c3ead1cc13111906ae2ebb71e9bbdbf87c30
    Tracked-On: https://jira01.devtools.intel.com/browse/CAR-4528
    Category: device enablement
    Domain: AOSP.ART-Other
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Anuvarshini Bc <anuvarshini.bc@intel.com>
    Reviewed-on: https://android.intel.com/617413
    Reviewed-by: Kumar Gupta, Sanjiv <sanjiv.kumar.gupta@intel.com>
    Tested-by: Kumar Gupta, Sanjiv <sanjiv.kumar.gupta@intel.com>

---
+++ test/8111-LoopPeeling-SetterGetter-01/src/Cls1.java
@@ -0,0 +1,16 @@
+
+public class Cls1{
+    public static int s_field1 = 33;
+    public static double s_field2 = -32.012345678d;
+    public float i_field1 = 102345678.123f;
+    public double i_field3 = 102345678.123d;
+    public long i_field2 = 0xFFFFFFFFL;
+    public int[] arr_field1 = new int[10];
+    public static int[] arr_field2 = {0,1,2,3,4,5,6,7,8,9};
+    public static String str_field = "my string";
+    public volatile int i_v_field = 0;
+    public static volatile int s_v_field = 1;
+
+
+
+}
